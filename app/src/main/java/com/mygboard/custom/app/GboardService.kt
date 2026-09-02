db489b4606e96f56992346f693c7c415c6ddd0f7
sjskoelfkxaajekjfjds-dev <sjskoelfkxaajekjfjds@gmail.com>
2026-09-02 15:17:56 +0300

    Update GboardService.kt

--- app/src/main/java/com/mygboard/custom/app/GboardService.kt
+++ app/src/main/java/com/mygboard/custom/app/GboardService.kt
@@ -4,6 +4,7 @@ import android.inputmethodservice.InputMethodService
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import com.mygboard.custom.app.R
 
 class GboardService : InputMethodService() {
 
